bfab2868453c3030390c21d66256c2d15ccd4232
Rafael L. Cintra <rcintra@pobox.com>
2018-01-18 19:27:05 +1000

    Support for plataform independent directory separator.

--- src/RPKI/Repository/Server.hs
+++ src/RPKI/Repository/Server.hs
@@ -27,6 +27,7 @@ import RPKI.Repository.Rsync
 import RPKI.Repository.XML
 import System.Directory (doesFileExist, createDirectoryIfMissing, createDirectory, getFileSize,
                          removeFile, removeDirectoryRecursive, doesDirectoryExist, listDirectory)
+import System.FilePath (pathSeparator)
 import System.Log.FastLogger
 import System.Log.FastLogger.Date
 import Text.Read (readMaybe)
@@ -213,10 +214,10 @@ startPeriodicRepoCleanup c state = cleanup (0, 0)
 
 -- Paths
 getNotificationPath :: Config -> FilePath
-getNotificationPath c = publicationPath c ++ "/notification.xml"
+getNotificationPath c = publicationPath c +/ "notification.xml"
 
 getSnapshotPath :: Config -> S.SessionId -> S.Serial -> FilePath
-getSnapshotPath c sId serial = getSerialPath c (B.unpack sId ) serial ++ "/snapshot.xml"
+getSnapshotPath c sId serial = getSerialPath c (B.unpack sId ) serial +/ "snapshot.xml"
 
 getPathForSnapshot :: Config -> S.Snapshot -> FilePath
 getPathForSnapshot c s = getSnapshotPath c (S.sessionId s) (S.serial s)
@@ -225,16 +226,19 @@ doesNotificationFileExist :: Config -> IO Bool
 doesNotificationFileExist = doesFileExist . getNotificationPath
 
 getSessionPath :: Config -> N.SessionId -> FilePath
-getSessionPath c sId = publicationPath c ++ "/" ++ sId
+getSessionPath c sId = publicationPath c +/ sId
 
 getSerialPath :: Config -> N.SessionId -> N.Serial -> FilePath
-getSerialPath c sId serial = getSessionPath c sId ++ "/" ++ show serial
+getSerialPath c sId serial = getSessionPath c sId +/show serial
 
 getDeltaPath :: Config -> N.SessionId -> Delta.Serial -> FilePath
-getDeltaPath c sId serial = getSerialPath c sId serial ++ "/delta.xml"
+getDeltaPath c sId serial = getSerialPath c sId serial +/ "delta.xml"
 
 getPathForDelta :: Config -> Delta.Delta -> FilePath
 getPathForDelta c d = getDeltaPath c (Delta.sessionId d) (Delta.serial d)
+
+(+/) :: String -> String -> String
+(+/) s1 s2 = s1 ++ [pathSeparator] ++ s2
 
 -- URIs
 snapshotURI :: Config -> S.SessionId -> S.Serial -> N.URI
